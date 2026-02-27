041a219433817290df10c80705b924f60f776a17
farhan <farhanbajwa46@gmail.com>
2026-02-27 17:21:46 +0500

    feat(posts): add throttler guard

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,18 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [
+    PostsService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class PostsModule {}
